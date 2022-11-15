eaf4790004e7b5dac6e1743a4ca6eaf3d48f9a02
Edwin Boon <edwin.boon@eo.nl>
2022-11-15 13:45:22 +0100

    Add action to update items from in the cart

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -9,7 +9,14 @@ export const useCartStore = defineStore("CardStore", {
   getters: {
     count: (state) => state.items.length,
     isEmpty: (state) => state.count === 0,
-    grouped: (state) => groupBy(state.items, (item) => item.name),
+    grouped: (state) => {
+      const grouped = groupBy(state.items, (item) => item.name);
+      const sorted = Object.keys(grouped).sort();
+      let inOrder = {};
+
+      sorted.forEach((key) => (inOrder[key] = grouped[key]));
+      return inOrder;
+    },
     groupCount: (state) => (name) => state.grouped[name].length,
     total: (state) => state.items.reduce((p, c) => p + c.price, 0),
   },
@@ -22,6 +29,11 @@ export const useCartStore = defineStore("CardStore", {
     },
     clearItem(name) {
       this.items = this.items.filter((item) => item.name !== name);
+    },
+    updateItem(item, count) {
+      count = parseInt(count);
+      this.clearItem(item.name);
+      this.addItems(count, item);
     },
   },
 });
